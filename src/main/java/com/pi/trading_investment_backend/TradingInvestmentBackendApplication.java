293292949c737e7ba20a43b2677f2d761da7a17b
chihebkitar <chihebkitar11@gmail.com>
2024-11-28 20:00:22 +0100

    init commit

---
+++ src/main/java/com/pi/trading_investment_backend/TradingInvestmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pi.trading_investment_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingInvestmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingInvestmentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/trading_investment_backend/TradingInvestmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.trading_investment_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingInvestmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
